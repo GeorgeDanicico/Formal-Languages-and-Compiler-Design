d11dd05a4fb930acc86efca46b238f3919bbe50a
GeorgeDanicico <georgedanicico@yahoo.com>
2022-12-01 13:04:16 +0200

    lab5 delivarables

---
+++ Lab5/src/common/Pair.java
@@ -0,0 +1,48 @@
+package common;
+
+import java.util.Objects;
+
+public class Pair<T, V> {
+    private T first;
+    private V second;
+
+    public Pair(T first, V second) {
+        this.first = first;
+        this.second = second;
+    }
+
+
+    public T getFirst() {
+        return first;
+    }
+
+    public void setFirst(T first) {
+        this.first = first;
+    }
+
+    public V getSecond() {
+        return second;
+    }
+
+    public void setSecond(V second) {
+        this.second = second;
+    }
+
+    @Override
+    public String toString() {
+        return "(First: " + first + ", Second: " + second + ")";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Pair<T, V> pair = (Pair<T, V>) o;
+        return Objects.equals(first, pair.first) && Objects.equals(second, pair.second);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(first, second);
+    }
+}
