4b3980b3cccd4d022f935de10b5864d2a8da428a
GeorgeDanicico <georgedanicico@yahoo.com>
2022-12-13 15:40:13 +0200

    lab6

---
+++ Lab6/src/Main.java
@@ -0,0 +1,57 @@
+import model.FiniteAutomaton;
+import model.Grammar;
+import model.JavaScanner;
+import model.SymbolTable;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void showMenu() {
+        System.out.println("Menu: ");
+        System.out.println("1. Print the set of terminals.");
+        System.out.println("2. Print the set of nonterminals.");
+        System.out.println("3. Print the set of productions.");
+        System.out.println("4. Print the produtions for a given nonterminal.");
+        System.out.println("5. CFG Check");
+        System.out.println("0. Exit");
+    }
+
+    public static void main(String[] args) throws IOException {
+        Grammar grammar = Grammar.provideGrammar("src/io/bnf-test.txt");
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            showMenu();
+            System.out.println(">> ");
+            int command = Integer.parseInt(scanner.nextLine());
+
+            if (command == 0) break;
+
+            switch (command) {
+                case 1:
+                    System.out.println("The set of terminals is:\n" + grammar.getTerminals());
+                    break;
+                case 2:
+                    System.out.println("The set of nonterminals is:\n" + grammar.getNonTerminals());
+                    break;
+                case 3:
+                    System.out.println("The set of productions is:\n" + grammar.getProductions());
+                    break;
+                case 4:
+                    System.out.println("Insert a nonterminal>>");
+                    String nonTerminal = scanner.nextLine();
+                    System.out.println("The set of production for " + nonTerminal + " is: " +
+                            grammar.getProductions(nonTerminal));
+                    break;
+                case 5:
+                    System.out.println("Is this grammar a CFG? " + grammar.isCFG() +
+                            "\n");
+                    break;
+
+
+            }
+        }
+
+    }
+}
---
+++ Lab6/src/model/Grammar.java
@@ -0,0 +1,88 @@
+package model;
+
+import java.io.*;
+import java.util.*;
+
+public class Grammar {
+    private final String start;
+    private final Set<String> nonTerminals;
+    private final Set<String> terminals;
+    private final Map<String, Set<String>> productions;
+    private final boolean isCFG;
+
+    public static Grammar provideGrammar(String fileName) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName));
+        var nonTerminals = new HashSet<String>();
+        var terminals = new HashSet<String>();
+        var productions = new HashMap<String, Set<String>>();
+        var isCFG = true;
+        var line = bufferedReader.readLine();
+
+        if (line == null) {
+            throw new RuntimeException("The format of the grammar file is not valid!");
+        }
+
+        while (line != null) {
+            var tokens = line.split(" = ");
+
+            if (tokens.length != 2) {
+                throw new RuntimeException("The format of the grammar file is not valid!");
+            }
+
+            var nonTerminal = tokens[0].trim();
+            if (nonTerminal.split(" ").length != 1) isCFG = false;
+            nonTerminals.add(nonTerminal);
+
+            var currentProductions = tokens[1].split("\\|");
+            for (var production: currentProductions) {
+                productions.computeIfAbsent(nonTerminal, k -> new HashSet<>());
+                productions.get(nonTerminal).add(production.trim());
+
+                var elements = production.split(" ");
+                for (var element: elements) {
+                    var length = element.length();
+                    if (length >= 3 && element.charAt(0) == '"' && element.charAt(length - 1) == '"') {
+                        terminals.add(element.trim());
+                    }
+                }
+            }
+
+            line = bufferedReader.readLine();
+        }
+
+        return new Grammar(line, nonTerminals, terminals, productions, isCFG);
+    }
+
+    private Grammar(String start, Set<String> nonTerminals, Set<String> terminals, Map<String, Set<String>> productions,
+                    boolean isCFG) {
+        this.start = start;
+        this.nonTerminals = nonTerminals;
+        this.terminals = terminals;
+        this.productions = productions;
+        this.isCFG = isCFG;
+    }
+
+    public Set<String> getProductions(String nonTerminal) {
+        return productions.get(nonTerminal);
+    }
+
+    public String getStart() {
+        return start;
+    }
+
+    public Set<String> getNonTerminals() {
+        return nonTerminals;
+    }
+
+    public Set<String> getTerminals() {
+        return terminals;
+    }
+
+    public Map<String, Set<String>> getProductions() {
+        return productions;
+    }
+
+    public boolean isCFG() {
+        return isCFG;
+    }
+}
